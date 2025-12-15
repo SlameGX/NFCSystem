c4d68d1502875c624c3c733adea17c6feb4f1e39
Elxan Kerimov <elxan.kerimov0055@gmail.com>
2025-12-15 13:01:03 +0400

    asdasfdg

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://51.12.223.183:5000/api/login', formData);
+            const res = await axios.post('/api/login', formData);
             if (res.data.success) {
                 localStorage.setItem('isAuthenticated', 'true');
                 navigate('/dashboard');
